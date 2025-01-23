29cc98b46c6d521013e92ce399406db767345d84
John Benio <johnbenio250@gmail.com>
2025-01-23 22:41:54 +0100

    Add book form created

--- Frontend/src/app/book/add-book/add-book.component.ts
+++ Frontend/src/app/book/add-book/add-book.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';  // <-- Import CommonModule
 
 @Component({
   selector: 'app-add-book',
   templateUrl: './add-book.component.html',
+  imports: [FormsModule, CommonModule],
   styleUrls: ['./add-book.component.css']
 })
 export class AddBookComponent implements OnInit {
-
+  @ViewChild('Form') addBookForm!: NgForm;
   constructor(private router: Router) { }
 
   ngOnInit() {
@@ -17,4 +20,7 @@ export class AddBookComponent implements OnInit {
     this.router.navigate(['/']);
   }
 
+  onSubmit(): void{
+    console.log("Form submitted!", this.addBookForm);
+  }
 }
