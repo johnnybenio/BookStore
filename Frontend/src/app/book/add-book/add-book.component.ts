54a238514bf4442e39a2d8c5ff89b11169c3bb6f
John Benio <johnbenio250@gmail.com>
2025-02-25 18:20:11 +0100

    working on the add book form design

--- Frontend/src/app/book/add-book/add-book.component.ts
+++ Frontend/src/app/book/add-book/add-book.component.ts
@@ -2,16 +2,26 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';  // <-- Import CommonModule
-import { TabsModule } from 'ngx-bootstrap/tabs';
+import { TabsetComponent, TabsModule } from 'ngx-bootstrap/tabs';
+import { BookCardComponent } from "../book-card/book-card.component";
+import { IBook } from '../ibook';
 
 @Component({
   selector: 'app-add-book',
   templateUrl: './add-book.component.html',
-  imports: [FormsModule, CommonModule, TabsModule],
+  imports: [FormsModule, CommonModule, TabsModule, BookCardComponent],
   styleUrls: ['./add-book.component.css']
 })
 export class AddBookComponent implements OnInit {
   @ViewChild('Form') addBookForm!: NgForm;
+  @ViewChild('formTabs') formTabs?: TabsetComponent;
+
+  bookView: IBook = {
+    Id: 0,
+    Name: '',
+    Price: 0
+  };
+
   constructor(private router: Router) { }
 
   ngOnInit() {
@@ -23,5 +33,11 @@ export class AddBookComponent implements OnInit {
 
   onSubmit(): void{
     console.log("Form submitted!", this.addBookForm);
+  }
+
+  selectTab(tabId: number) {
+    if (this.formTabs?.tabs[tabId]) {
+      this.formTabs.tabs[tabId].active = true;
+    }
   }
 }
