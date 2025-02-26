c0bb88239b6cf2c73a047be01cc68cf44ff40dcc
John Benio <johnbenio250@gmail.com>
2025-02-26 23:30:35 +0100

    Make changes to enhance the add book form view

--- Frontend/src/app/book/book-card/book-card.component.ts
+++ Frontend/src/app/book/book-card/book-card.component.ts
@@ -1,14 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { IBook } from '../ibook';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-book-card',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './book-card.component.html',
   styleUrl: './book-card.component.css'
 })
 export class BookCardComponent {
 @Input() book!: IBook;
-
+@Input() hideIcons!: boolean;
 }
